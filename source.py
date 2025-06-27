6b8f8bb866901a1fbc933d3963ce5ca6873a66a3
RSuryaa <105858699+RSuryaa@users.noreply.github.com>
2025-06-27 19:27:24 +0530

    Update source.py 2.0

--- source.py
+++ source.py
@@ -7,3 +7,7 @@ def encrypt_message(string_message, key):
 def decrypt_message(message_encrypted_version, key):
     fernet = Fernet(key)
     return fernet.decrypt(message_encrypted_version).decode()
+
+def random_key_generator():
+    key = Fernet.generate_key()
+    return key
